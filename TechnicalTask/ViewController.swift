ec34165775049a842797622628fa79af165bc7ea
vetka4ever <dansh@sfedu.ru>
2022-11-28 18:06:44 +0300

    first commit

---
+++ TechnicalTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnicalTask
+//
+//  Created by Daniil on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // test git
+    }
+
+
+}
+
